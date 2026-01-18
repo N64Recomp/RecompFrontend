984339710bac24e266bcf4743c9670f7268d40f0
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2026-01-18 18:08:25 -0500

    Fix mipmap bias

--- recompui/src/renderer/ui_renderer.cpp
+++ recompui/src/renderer/ui_renderer.cpp
@@ -192,7 +192,7 @@ public:
 
         samplerDesc.minFilter = plume::RenderFilter::LINEAR;
         samplerDesc.magFilter = plume::RenderFilter::LINEAR;
-        samplerDesc.mipLODBias = 0.25f;
+        samplerDesc.mipLODBias = -0.5f;
         linearSampler_ = device_->createSampler(samplerDesc);
 
         // Create the shaders
